d60749a4b111e6cac96ba5820e86ef8d0e3c171d
ralbasini <romain.albasini@live.com>
2026-05-20 19:44:21 +0200

    Add CNAME

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: 'src',
-  base: process.env.NODE_ENV === 'production' ? '/parenthese-hygieniste/' : '/',
+  base: '/',
   publicDir: '../public',
   build: {
     outDir: '../dist',
